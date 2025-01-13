8806474118b61a0030052c4af0d217ce4603a381
Taran Sahota <tazsahota2003@gmail.com>
2025-01-13 15:32:32 +0000

    week 2 day 1 loops workshop

---
+++ app.js
@@ -0,0 +1,25 @@
+
+console.log("hello world")
+
+for (let i = 0; i < 5; i++) {
+    console.log(i)
+}
+
+//for i = 0 to i is less than 5, console log i, then add 1 
+
+let counter = 1
+while (counter < 6) {
+    console.log(counter)
+    counter += 1
+}
+
+let keeplooping = true
+let i = 0
+
+/*
+while (keeplooping == true); {
+    console.log("iteration ",i);
+    keeplooping=Math.random() < 0.5;
+    i++
+}
+*/
